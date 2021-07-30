df4471bb6cadbc0f3e53e7a3774690f19a7e023d
Koyel1234 <76770146+Koyel1234@users.noreply.github.com>
2021-07-31 02:11:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -100,7 +100,7 @@ if __name__=='__main__':
     parser.add_argument("-p", "--port", type=int, default=8080)
     args = parser.parse_args()
     
-    start_server(predict, port=args.port)
+    start_server(result, port=args.port)
     
     
 #app.add_url_rule('/tool','webio_view',webio_view(result),methods=['GET','POST','OPTIONS'])    
